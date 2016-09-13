cee2d2be5238b9568cc14e3ee9f39407bfbb9036
Yannick Armand (PifyZ) <yannick.armand@gmail.com>
2016-09-13 19:41:01 +0200

    Ajout des diverses armes

--- Weapon.elm
+++ Weapon.elm
@@ -38,6 +38,39 @@ newWeapon =
   }
 
 
+newDynamite : Weapon
+newDynamite =
+  { newWeapon
+    | name = "Dynamite"
+    , ammo = 1000
+    , baseLatency = 5
+    , miniatureIndex = 0
+    , weaponType = Dynamite
+    }
+
+
+newFlameThrower : Weapon
+newFlameThrower =
+  { newWeapon
+    | name = "Lance-flammes"
+    , ammo = 1000
+    , baseLatency = 5
+    , miniatureIndex = 0
+    , weaponType = FlameThrower
+    }
+
+
+newGrenade : Weapon
+newGrenade =
+  { newWeapon
+    | name = "Grenade"
+    , ammo = 1000
+    , baseLatency = 5
+    , miniatureIndex = 0
+    , weaponType = Grenade
+    }
+
+
 newGun : Weapon
 newGun =
   { newWeapon
@@ -46,4 +79,59 @@ newGun =
     , baseLatency = 5
     , miniatureIndex = 0
     , weaponType = Gun
+    }
+
+
+newMine : Weapon
+newMine =
+  { newWeapon
+    | name = "Mine"
+    , ammo = 1000
+    , baseLatency = 5
+    , miniatureIndex = 0
+    , weaponType = Mine
+    }
+
+
+newRocketLauncher : Weapon
+newRocketLauncher =
+  { newWeapon
+    | name = "Lance-roquettes"
+    , ammo = 1000
+    , baseLatency = 5
+    , miniatureIndex = 0
+    , weaponType = RocketLauncher
+    }
+
+
+newShotgun : Weapon
+newShotgun =
+  { newWeapon
+    | name = "Fusil à pompe"
+    , ammo = 1000
+    , baseLatency = 5
+    , miniatureIndex = 0
+    , weaponType = Shotgun
+    }
+
+
+newSmokeGrenade : Weapon
+newSmokeGrenade =
+  { newWeapon
+    | name = "Fumigène"
+    , ammo = 1000
+    , baseLatency = 5
+    , miniatureIndex = 0
+    , weaponType = SmokeGrenade
+    }
+
+
+newSubmachineGun : Weapon
+newSubmachineGun =
+  { newWeapon
+    | name = "Mitraillette"
+    , ammo = 1000
+    , baseLatency = 5
+    , miniatureIndex = 0
+    , weaponType = SubmachineGun
     }
